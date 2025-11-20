9c5bf14fb7f30ee3442594296fb75f52b540a163
alex cappelletti <cappelle@fbk.eu>
2025-11-20 10:00:18 +0100

    try to understand keys on real deploy2

--- msp-analyzer/server/api/scenario-provider.post.ts
+++ msp-analyzer/server/api/scenario-provider.post.ts
@@ -4,15 +4,11 @@ export default defineEventHandler(async (event) => {
 	const body = await readBody(event);
 	const scenarioID: string | undefined =
 		typeof body.scenarioID === 'string' ? body.scenarioID : undefined;
-	const publicUrl = 'http://localhost:3000/fixtures/scenario_bd-v0_02.json';
+	const fileUrl = '/fixtures/scenario_bd-v0_02.json';
     console.log('Received request for scenario ID:', scenarioID || 'N/A');
 	try {
-		// Usa useStorage().getItem() per leggere il file.
-		// ad esempio nella root (se configurata per storage) o nella cartella 'public'.
-		// const data = await $fetch(publicUrl);
-		const storageKey = 'root:public:fixtures:scenario_bd-v0_02.json';
-		const data = await useStorage().getItem(storageKey) ;
-		if (!data) {throw new Error('Impossibile caricare i dati dello scenario.');}
+		const data = await $fetch(fileUrl) ;
+		if (!data) {throw new Error('Impossibile caricare i dati dello scenario.' + fileUrl);}
 		return {scenario: data,};
 	} catch (error) {
 		// Gestione degli errori: restituisce una risposta di errore standard di Nuxt
@@ -20,7 +16,7 @@ export default defineEventHandler(async (event) => {
 		return createError({
 			statusCode: 500,
 			statusMessage:
-				'Internal Server Error: Impossibile leggere il file dei dati (fetch).',
+				'Internal Server Error: Impossibile leggere il file dei dati (fetch).' ,
 		});
 	}
 });
